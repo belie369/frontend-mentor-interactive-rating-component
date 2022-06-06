42a436ae869576964353f948af82bc6ea2aeb415
belie369 <katarzyna.derbich@gmail.com>
2022-06-06 13:24:17 +0200

    Adding functionality: validation of the selection of rating; changing a color when selecting a circle; passing a value to a selectedValue variable; showing a selected rating value

--- script/script.js
+++ script/script.js
@@ -2,14 +2,37 @@ const componentEvaluation = document.querySelector('.component__evaluation');
 const componentThankyou = document.querySelector('.component__thankyou');
 const numberBtns = document.querySelectorAll('.component__evaluation-circle');
 const submitBtn = document.querySelector('.component__evaluation-submit');
+const thankyouRating = document.querySelector('.component__thankyou-rating');
+
+let selectedValue = 0;
+
+const checkCircle = (e) => {
+	numberBtns.forEach((el) => el.classList.remove('component__evaluation-circle--selected'));
+	e.target.classList.toggle('component__evaluation-circle--selected');
+	selectValue(e);
+	showScore();
+};
+
+const selectValue = (e) => {
+	selectedValue = e.target.value;
+};
+
+const showScore = () => {
+	thankyouRating.textContent = `${selectedValue}`;
+};
 
 const showThankyou = () => {
-	componentEvaluation.classList.toggle('fade-out');
-	setTimeout(() => {
-		componentEvaluation.style.display = 'none';
-		componentThankyou.classList.toggle('fade-in');
-		componentThankyou.style.display = 'block';
-	}, 200);
+	if (selectedValue <= 0) {
+		alert('Please select a rating!');
+	} else {
+		componentEvaluation.classList.toggle('fade-out');
+		setTimeout(() => {
+			componentEvaluation.style.display = 'none';
+			componentThankyou.classList.toggle('fade-in');
+			componentThankyou.style.display = 'block';
+		}, 200);
+	}
 };
 
 submitBtn.addEventListener('click', showThankyou);
+numberBtns.forEach((el) => el.addEventListener('click', checkCircle));
